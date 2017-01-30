5a98d7b84bb0b351019f070b923701ae3b170e6a
Matt Molo <molo.matt@gmail.com>
2017-01-30 08:18:25 -0500

    Add other components

---
+++ client/src/components/logout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import cookie from 'react-cookie'
+
+
+export default class Logout extends React.Component {
+    render() {
+        return (
+            <div>Logging out...</div>
+        );
+    }
+}
