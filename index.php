f3a4b9632438e50a74c6092cd781734c8e027828
Kyle Rakos <rakoskr6@gmail.com>
2017-08-10 18:00:14 -0400

    Updated login UI to make Brandon happy

--- index.php
+++ index.php
@@ -1,47 +1,49 @@
 <?php
-$title = 'Boiler Books';
-include 'header.php';
-include 'dbinfo.php';
-$returnto = test_input2($_GET['returnto']);
-$verifylogin = "login.php?returnto=" . $returnto;
-include 'assets/fakemenu.php'
-?>
-
+	$title = 'Boiler Books';
+	include 'header.php';
+	include 'dbinfo.php';
+	$returnto = test_input2($_GET['returnto']);
+	$verifylogin = "login.php?returnto=" . $returnto;
+	include 'assets/fakemenu.php'
+	?>
 
 <div class="container">
-
-	<form class="form-signin" action=<?php echo $verifylogin ?> method="post">
-		<h3 class="form-signin-heading">Please Sign In</h3>
-		<font color="blue"><em><?php echo test_input($_GET["fail"]); ?></em></font>
-		<label for="inputEmail" class="sr-only">Username</label>
-		<input type="text" id="usr" name="usr" class="form-control" placeholder="Your Boiler Books user account" required autofocus>
-		<label for="inputPassword" class="sr-only">Password</label>
-		<input type="password" id="psw" name = "psw" class="form-control" placeholder="Password" required>
-
-
-		<div class="form-group">
-			<button id="submit" name="submit" class="btn btn-primary">Sign in</button>
+		<div class="col-sm-3">
 		</div>
+		<div class="col-sm-6">
+			<form class="form-signin" action=<?php echo $verifylogin ?> method="post">
+				<h3 class="form-signin-heading">Please Sign In</h3>
+				<font color="blue"><em><?php echo test_input($_GET["fail"]); ?></em></font>
+				
+				<label for="inputEmail" class="sr-only">Username</label>
+				<input type="text" id="usr" name="usr" class="form-control" placeholder="Your Boiler Books user account" required autofocus>
 
+				<label for="inputPassword" class="sr-only">Password</label>
+				<input type="password" id="psw" name = "psw" class="form-control" placeholder="Password" required>
+				
+				<br>
 
-		<a href = "user/newuser.php" class="btn btn-lg btn-block">New User</a></button>
-		<br>
-		<div class = "container">
-			<div class="col-sm-6">
-				<a href = "user/forgotusername.php" class="btn btn-lg btn-block">Forgot username?</a></button>
-			</div>
-			<div class="col-sm-6">
-				<a href = "user/forgotpassword.php" class="btn btn-lg btn-block">Forgot password?</a></button>
-			</div>
+				<div class="form-group">
+					<button id="submit" name="submit" class="btn btn-primary">Sign in</button>
+				</div>
+				
+
+				<a href = "user/newuser.php" class="btn btn-lg btn-block">New User</a></button>
+				<br>
+				<div>
+					<div class="col-sm-6">
+						<a href = "user/forgotusername.php" class="btn btn-lg btn-block">Forgot username?</a></button>
+					</div>
+					<div class="col-sm-6">
+						<a href = "user/forgotpassword.php" class="btn btn-lg btn-block">Forgot password?</a></button>
+					</div>
+				</div>
+			</form>
 		</div>
-
-	</form>
-
-</div> <!-- /container -->
-
-
-
-
+		<div class="col-sm-3">
+		</div>
+	</div> 
+<!-- /container -->
 <?php
-include 'smallfooter.php';
-?>
+	include 'smallfooter.php';
+	?>
