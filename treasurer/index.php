967d5a8697423f4de787df69768fc58c2a8a14ed
Brandon Stewart <stewartb2@gmail.com>
2017-08-10 22:24:07 -0500

    Add selection of different committees and years for treasurer (#59)
    Add selection of different committees and years for treasurer

--- treasurer/index.php
+++ treasurer/index.php
@@ -1,7 +1,7 @@
 <?php
-	$title = 'Boiler Books';
-	$treasuereactive = "active";
-	include '../menu.php';
+    $title = 'Boiler Books';
+    $treasuereactive = "active";
+    include '../menu.php';
 
 ?>
 
@@ -11,12 +11,24 @@ include '../dbinfo.php';
 $items = '';
 $usr = $_SESSION['user'];
 
+$committee = test_input($_GET["committee"]);
+if ($committee == '') {
+   $committee = '%';
+}
+
+
+$fiscalyear = test_input($_GET["fiscalyear"]);
+if ($fiscalyear == '') {
+   $fiscalyear = '2016-2017';
+}
+
+echo $commmittee;
 
 try {
-	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-	// set the PDO error mode to exception
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$sql = "SELECT DATE_FORMAT(p.purchasedate,'%m/%d/%Y') as date
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "SELECT DATE_FORMAT(p.purchasedate,'%m/%d/%Y') as date
 , p.item
 , p.purchaseID
 , p.purchasereason
@@ -37,69 +49,70 @@ AND '$usr' in (
     SELECT U3.username FROM Users U3
     INNER JOIN approval A ON U3.username = A.username
     WHERE (A.role = 'treasurer' OR A.role = 'president'))
+AND p.committee LIKE '$committee' AND p.fiscalyear LIKE '$fiscalyear'
 
 ORDER BY p.purchasedate DESC";
-	//$stmt->execute();
+    //$stmt->execute();
 
 
-	foreach ($conn->query($sql) as $row) {
-		$items .= '<tr> <td><a href=/purchase.php?purchaseid=';
-		$items .= $row['purchaseID'];
-		$items .= '>';
-		$items .= $row['purchaseID'];
-		$items .= '</a>';
-		$items.= '</td> <td>';
-		$items .= $row['date'];
-		$items .= '</td> <td><a href=';
-		$items .= $row['receipt'];
-		$items .= '>';
-		$items .= $row['item'];
-		$items .= '</a></td> <td>';
-		$items .= $row['fundsource'];
-		$items .= '</td> <td>';
-		$items .= $row['vendor'];
-		$items .= '</td> <td>';
-		$items .= $row['committee'];
-		$items .= "</td> <td><a href='user.php?usrlookup=";
-		$items .= $row['username'];
-		$items .= "'>";
-		$items .= $row['purchasedby'];
-		$items .= '</a></td> <td>';
-		$items .= $row['status'];
-		$items .= '</td> <td>';
-		$items .= $row['cost'];
-		$items .= '</td> <td>';
-		$items .= $row['comments'];
+    foreach ($conn->query($sql) as $row) {
+        $items .= '<tr> <td><a href=/purchase.php?purchaseid=';
+        $items .= $row['purchaseID'];
+        $items .= '>';
+        $items .= $row['purchaseID'];
+        $items .= '</a>';
+        $items.= '</td> <td>';
+        $items .= $row['date'];
+        $items .= '</td> <td><a href=';
+        $items .= $row['receipt'];
+        $items .= '>';
+        $items .= $row['item'];
+        $items .= '</a></td> <td>';
+        $items .= $row['fundsource'];
+        $items .= '</td> <td>';
+        $items .= $row['vendor'];
+        $items .= '</td> <td>';
+        $items .= $row['committee'];
+        $items .= "</td> <td><a href='user.php?usrlookup=";
+        $items .= $row['username'];
+        $items .= "'>";
+        $items .= $row['purchasedby'];
+        $items .= '</a></td> <td>';
+        $items .= $row['status'];
+        $items .= '</td> <td>';
+        $items .= $row['cost'];
+        $items .= '</td> <td>';
+        $items .= $row['comments'];
 
 
-		$items .= "</td> <td><a href='update.php?reimbursed=-1&processing=";
-		$items .= $row['purchaseID'];
-		$items .= "'>Mark Processing";
-		$items .= '</a></td> <td>';
+        $items .= "</td> <td><a href='update.php?reimbursed=-1&processing=";
+        $items .= $row['purchaseID'];
+        $items .= "'>Mark Processing";
+        $items .= '</a></td> <td>';
 
-		$items .= "<a href='update.php?processing=-1&reimbursed=";
-		$items .= $row['purchaseID'];
-		$items .= "'>Mark Reimbursed";
-		$items .= '</a></td>';
-
-
+        $items .= "<a href='update.php?processing=-1&reimbursed=";
+        $items .= $row['purchaseID'];
+        $items .= "'>Mark Reimbursed";
+        $items .= '</a></td>';
 
 
 
-		$items .= "</fieldset>
-		</form>";
-		$items .= '</td></tr>';
 
 
-	}
-		//echo $items;
+        $items .= "</fieldset>
+        </form>";
+        $items .= '</td></tr>';
 
 
-	}
+    }
+        //echo $items;
+
+
+    }
 catch(PDOException $e)
-	{
-	echo $sql . "<br>" . $e->getMessage();
-	}
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
 
 $conn = null;
 ?>
@@ -109,12 +122,12 @@ $conn = null;
 <div class="container">
     <div class="row">
         <div class="col-sm-6">
-            <select id="committee" name="committee" class="form-control" onchange="selectcommittee()">
+            <select id="committee" name="committee" class="form-control" onchange="selectcommitteeyear()">
               <?php include '../committees.php'; ?>
             </select>
         </div>
         <div class="col-sm-6">
-            <select id="fiscalyear" name="fiscalyear" class="form-control" onchange="selectyear()">
+            <select id="fiscalyear" name="fiscalyear" class="form-control" onchange="selectcommitteeyear()">
                 <option value="2017-2018">Select Year</option>
                 <option value="2017-2018">2017 - 2018</option>
                 <option value="2016-2017">2016 - 2017</option>
@@ -127,41 +140,61 @@ $conn = null;
 <br>
 
 <div class="container">
-	<table id="treasurertable" class="display">
-		<thead>
-			<tr>
-				<th>Purchase ID</th>
-				<th>Purchase Date</th>
-				<th>Item</th>
-				<th>Fund Source</th>
-				<th>Vendor</th>
-				<th>Committee</th>
-				<th>Purchased By</th>
-				<th>Status</th>
-				<th>Amount</th>
-				<th>Comments</th>
-				<th>Processing</th>
-				<th>Reimbursed</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php echo $items ?>
-		</tbody>
-	</table>
-	<script>
-	$(document).ready(function() {
-	    $('#treasurertable').DataTable( {
-	        "order": [[ 1, "desc" ]]
-	    } );
-			stateSave: true 
+    <table id="treasurertable" class="display">
+        <thead>
+            <tr>
+                <th>Purchase ID</th>
+                <th>Purchase Date</th>
+                <th>Item</th>
+                <th>Fund Source</th>
+                <th>Vendor</th>
+                <th>Committee</th>
+                <th>Purchased By</th>
+                <th>Status</th>
+                <th>Amount</th>
+                <th>Comments</th>
+                <th>Processing</th>
+                <th>Reimbursed</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php echo $items ?>
+        </tbody>
+    </table>
+    <script>
+    $(document).ready(function() {
+        $('#treasurertable').DataTable( {
+            "order": [[ 1, "desc" ]]
+        } );
+            stateSave: true
 
-	} );
-	</script>
+    } );
+
+    function selectcommitteeyear() {
+
+        var com = document.getElementById('committee').value;
+        if (com == '') {
+            com = "<?php echo $committee ?>";
+        }
+            var title = "index.php?committee=";
+            var partial  = title.concat(com);
+            var com2 = document.getElementById('fiscalyear').value;
+        if (com2 == '') {
+            com2 = "<?php echo $fiscalyear ?>";
+        }
+            var fiscalyear = "&fiscalyear=";
+            var tempFinal = fiscalyear.concat(com2);
+            fullFinal = partial.concat(tempFinal);
+
+            window.location = fullFinal;
+    }
+
+    </script>
 </div>
 
 
 
 
 <?php
-	include '../smallfooter.php';
+    include '../smallfooter.php';
 ?>
