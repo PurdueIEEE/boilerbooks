7358440aa63e6dd6e74f9af42fa7e459b66f5e56
Grant Geyer <GrantG012@yahoo.com>
2021-02-11 18:17:25 -0500

    Keep anything the user has entered in the request inputs

--- request/index.php
+++ request/index.php
@@ -47,6 +47,22 @@ include '../dbinfo.php';
     }
 }
 
+$item = $reason = $vendor = $cost = $comments = '';
+if(isset($_GET["item"])) {
+  $item = test_input($_GET["item"]);
+}
+if(isset($_GET["reason"])) {
+  $reason = test_input($_GET["reason"]);
+}
+if(isset($_GET["vendor"])) {
+  $vendor = test_input($_GET["vendor"]);
+}
+if(isset($_GET["cost"])) {
+  $cost = test_input($_GET["cost"]);
+}
+if(isset($_GET["comments"])) {
+  $comments = test_input($_GET["comments"]);
+}
 
 
 $categorylist = '';
@@ -105,35 +121,35 @@ $conn = null;
     <div class="form-group">
       <label class="col-md-4 control-label" for="item">Item Being Purchased</label>
       <div class="col-md-4">
-        <input id="item" name="item" type="text" placeholder="Resistors, screws, etc." class="form-control input-md" required="" maxlength="100">
+        <input id="item" name="item" type="text" placeholder="Resistors, screws, etc." class="form-control input-md" required="" maxlength="100" value="<?php echo $item; ?>">
       </div>
     </div>
 
     <div class="form-group">
       <label class="col-md-4 control-label" for="reason">Reason Being Purchased</label>
       <div class="col-md-4">
-        <input id="reason" name="reason" type="text" placeholder="For testing, building claw, etc." class="form-control input-md" required="">
+        <input id="reason" name="reason" type="text" placeholder="For testing, building claw, etc." class="form-control input-md" required="" value="<?php echo $reason; ?>">
       </div>
     </div>
 
     <div class="form-group">
       <label class="col-md-4 control-label" for="vendor">Vendor</label>
       <div class="col-md-4">
-        <input id="vendor" name="vendor" type="text" placeholder="McMaster, Digikey, etc." class="form-control input-md" required="">
+        <input id="vendor" name="vendor" type="text" placeholder="McMaster, Digikey, etc." class="form-control input-md" required="" value="<?php echo $vendor; ?>">
       </div>
     </div>
 
     <div class="form-group">
       <label class="col-md-4 control-label" for="cost">Cost</label>
       <div class="col-md-4">
-        <input id="cost" name="cost" type="number" step = "0.01" placeholder="$156.56" class="form-control input-md" required="">
+        <input id="cost" name="cost" type="number" step = "0.01" placeholder="$156.56" class="form-control input-md" required="" value="<?php echo $cost; ?>">
       </div>
     </div>
 
     <div class="form-group">
       <label class="col-md-4 control-label" for="comments">Comments</label>
       <div class="col-md-4">
-        <textarea class="form-control" id="comments" name="comments"></textarea>
+        <textarea class="form-control" id="comments" name="comments"><?php echo $comments; ?></textarea>
       </div>
     </div>
 
@@ -162,7 +178,27 @@ $conn = null;
   function categories() {
     var com = document.getElementById('committee').value;
     var title = "index.php?committee=";
-    var full = title.concat(com);
+    let item = document.getElementById("item").value;
+    if(item != '') {
+      item = "&item=" + item;
+    }
+    let reason = document.getElementById("reason").value;
+    if(reason != '') {
+      reason = "&reason=" + reason;
+    }
+    let vendor = document.getElementById("vendor").value;
+    if(vendor != '') {
+      vendor = "&vendor=" + vendor;
+    }
+    let cost = document.getElementById("cost").value;
+    if(cost != '') {
+      cost = "&cost=" + cost;
+    }
+    let comments = document.getElementById("comments").value;
+    if(comments != '') {
+      comments = "&comments=" + comments;
+    }
+    var full = title.concat(com, item, reason, vendor, cost, comments);
     window.location = full;
   }
 </script>
