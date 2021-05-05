c44e632707dc910197ae5d9c5de97df1a920471e
Grant Geyer <GrantG012@yahoo.com>
2021-05-05 17:40:13 -0400

    Automatically populate the committee in expenses if the user can only see one

--- committees.php
+++ committees.php
@@ -1,13 +1,13 @@
 <option value="">Select Committee</option>
-<option <?php echo $general ?> value="General IEEE">General IEEE</option>
-<option <?php echo $aerial ?> value="Aerial Robotics">Aerial Robotics</option>
-<option <?php echo $computer ?> value="Computer Society">Computer Society</option>
-<option <?php echo $embs ?> value="EMBS">EMBS</option>
-<option <?php echo $ge ?> value="GE">Growth and Engagement</option>
-<option <?php echo $mtts ?> value="MTT-S">MTT-S</option>
-<option <?php echo $professional ?> value="Professional">Professional</option>
-<option <?php echo $learning ?> value="Learning">Learning</option>
-<option <?php echo $racing ?> value="Racing">Racing</option>
-<option <?php echo $rov ?> value="ROV">ROV</option>
-<option <?php echo $social ?> value="Social">Social</option>
-<option <?php echo $soga ?> value="SOGA">SOGA</option>
+<option <?php echo $committee === "General IEEE"     ? "selected" : ''  ?> value="General IEEE">General IEEE</option>
+<option <?php echo $committee === "Aerial Robotics"  ? "selected" : '' ?> value="Aerial Robotics">Aerial Robotics</option>
+<option <?php echo $committee === "Computer Society" ? "selected" : '' ?> value="Computer Society">Computer Society</option>
+<option <?php echo $committee === "EMBS"             ? "selected" : '' ?> value="EMBS">EMBS</option>
+<option <?php echo $committee === "GE"               ? "selected" : '' ?> value="GE">Growth and Engagement</option>
+<option <?php echo $committee === "MTT-S"            ? "selected" : '' ?> value="MTT-S">MTT-S</option>
+<option <?php echo $committee === "Professional"     ? "selected" : '' ?> value="Professional">Professional</option>
+<option <?php echo $committee === "Learning"         ? "selected" : '' ?> value="Learning">Learning</option>
+<option <?php echo $committee === "Racing"           ? "selected" : '' ?> value="Racing">Racing</option>
+<option <?php echo $committee === "ROV"              ? "selected" : '' ?> value="ROV">ROV</option>
+<option <?php echo $committee === "Social"           ? "selected" : '' ?> value="Social">Social</option>
+<option <?php echo $committee === "SOGA"             ? "selected" : '' ?> value="SOGA">SOGA</option>
