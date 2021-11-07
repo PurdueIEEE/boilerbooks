884eefa79bb17c934e2b27f43e98029fbfbfbf7e
Grant Geyer <GrantG012@yahoo.com>
2021-11-07 17:14:07 -0600

    Fix missed variable rename

--- fiscal-year.php
+++ fiscal-year.php
@@ -8,8 +8,8 @@
     $g_first_fiscal_year = "2015-2016";
 
     $yearStart = intval(substr($g_first_fiscal_year, 0, 4));
-    $yearEnd = intval(substr($fiscal_year_file, 0, 4));
-    $g_year_options_select = "<option value=$fiscal_year_file >Select Year</option>";
+    $yearEnd = intval(substr($g_current_fiscal_year, 0, 4));
+    $g_year_options_select = "<option value=$g_current_fiscal_year >Select Year</option>";
     for($year = $yearEnd; $year >= $yearStart; $year--) {
         $nextYear = $year + 1;
         $g_year_options_select .= "<option value='$year-$nextYear'>$year - $nextYear</option>";
