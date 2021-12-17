5692dbb65a5371b07b3c04dda96085c98d805950
Grant Geyer <GrantG012@yahoo.com>
2021-12-17 11:08:57 -0600

    Force users to pick a committee when recording a donation

--- donation/index.php
+++ donation/index.php
@@ -16,7 +16,7 @@
 <div class="form-group">
   <label class="col-md-4 control-label" for="committee">Committee</label>
   <div class="col-md-4">
-    <select id="committee" name="committee" class="form-control">
+    <select id="committee" name="committee" class="form-control" required>
       <?php include '../committees.php'; ?>
     </select>
   </div>
