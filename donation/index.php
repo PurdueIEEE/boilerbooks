cda6e0f58e564233260d0d80dac21758fe7ffb4b
Grant Geyer <GrantG012@yahoo.com>
2021-12-30 17:02:50 -0600

    Transfer feature (#142)
    * Feature for making a transfered between committees (fixes #123)
    
    * Got it working on the server

--- donation/index.php
+++ donation/index.php
@@ -4,8 +4,8 @@
 	include '../menu.php';
 ?>
 
-
-	<form class="form-horizontal" action="donationprocessing.php" method="post">
+<div class="container">
+<form class="form-horizontal" action="donationprocessing.php" method="post">
 <fieldset>
 
 <!-- Form Name -->
@@ -25,8 +25,7 @@
 <div class="form-group">
   <label class="col-md-4 control-label" for="source">Source</label>
   <div class="col-md-4">
-  <input id="source" name="source" type="text" placeholder="Provost, PESC, Northrop, etc." class="form-control input-md" required="">
-
+    <input id="source" name="source" type="text" placeholder="Provost, PESC, Northrop, etc." class="form-control input-md" required>
   </div>
 </div>
 
@@ -34,8 +33,7 @@
 <div class="form-group">
   <label class="col-md-4 control-label" for="amount">Amount</label>
   <div class="col-md-4">
-  <input id="amount" name="amount" type="number" step="0.01" placeholder="$1500.00" class="form-control input-md" required="">
-
+    <input id="amount" name="amount" type="number" step="0.01" placeholder="$1500.00" class="form-control input-md" required>
   </div>
 </div>
 
@@ -43,8 +41,7 @@
 <div class="form-group">
   <label class="col-md-4 control-label" for="item">Item</label>
   <div class="col-md-4">
-  <input id="item" name="item" type="text" placeholder="If donation list item" class="form-control input-md">
-
+    <input id="item" name="item" type="text" placeholder="If donation list item" class="form-control input-md">
   </div>
 </div>
 
@@ -95,6 +92,62 @@
 </fieldset>
 </form>
 
+<div class="col-md-2"></div>
+<div class="col-md-8"><div class='panel panel-default'>
+    <div class='panel-heading'>
+        <h4 class='panel-title'>
+            <a data-toggle='collapse' href='#ca-transfer'>Inter-Committee Transfer</a>
+        </h4>
+    </div>
+    <div id='ca-transfer' class='panel-collapse collapse'>
+        <div class='panel-body'>
+            <form class="form-horizontal" action="/api/donation/transfer.php" method="post">
+
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="committee">Transfer from Committee</label>
+                    <div class="col-md-8">
+                        <select id="committee-transfer-from" name="committee-from" class="form-control" required>
+                            <?php include '../committees.php'; ?>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="committee">Transfer to Committee</label>
+                    <div class="col-md-8">
+                        <select id="committee-transfer-to" name="committee-to" class="form-control" required>
+                            <?php include '../committees.php'; ?>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="amount">Amount</label>
+                    <div class="col-md-8">
+                        <input id="amount" name="amount" type="number" step="0.01" placeholder="$1500.00" class="form-control input-md" required>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="comments">Comments</label>
+                    <div class="col-md-8">
+                        <textarea class="form-control" id="txt-comments-transfer" name="comments"></textarea>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="submit"></label>
+                    <div class="col-md-8">
+                        <button id="btn-submit-transfer" name="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </div>
+
+            </fieldset>
+            </form>
+        </div>
+    </div>
+</div></div>
+</div>
 
 
 <?php
