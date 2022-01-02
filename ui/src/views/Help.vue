752c7948ba8bb1e8bc0c93a64176fd5c81021738
Hadi Ahmed <hadiahmed098@gmail.com>
2022-01-01 22:10:37 -0600

    Added help page

--- ui/src/views/Help.vue
+++ ui/src/views/Help.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="container-lg my-5 pt-5">
-    <h1>This is a help page</h1>
+    <h1 class="mb-3">Boiler Books Help</h1>
+    <div class="text-start">
+    <p class="lead fs-3">Boiler Books is an expense and income tracking system.</p>
+    <p class="fs-5">
+    It was developed on behalf of IEEE to track all purchases for the organization.
+    The system allows any IEEE member to create an account and request to purchase
+    something on behalf of the indicated committee. The purchase may then be approved
+    or denied by the committee chair (or designated member). If the purchase is
+    approved, the member will then proceed to buy the item. After it is bought the
+    receipt shall be uploaded to allow for timely reimbursement by the IEEE treasurer.
+    </p>
+    <p class="fs-5">
+    In addition to being a convenient system to request and approve purchases, members
+    also have the ability to view all of their past purchases. Those with the appropriate
+    permissions are also able to view purchases and income for their entire committee.
+    </p>
+    <p class="fs-5">
+    Boiler Books is open source, licensed under the Apache 2.0 license, and under active
+    development. If there is a feature you would like to see, send us an email at
+    <a href="mailto:ieee-officers@purdueieee.org">ieee-officers@purdueieee.org</a>.
+    If you would like to help development, <a href="https://github.com/purdueieee/boilerbooks" target="_blank">visit our GitHub</a>!
+    </p>
+    <hr/>
+    <p class="lead fs-3">Purchasing Process</p>
+    <ol class="fs-5">
+      <li>Any user makes a purchase request, filling out details about the item and some tracking information</li>
+      <li>The committee chair or a designated committee member will review the request and approve or deny it</li>
+      <li>After successfully purchasing the approved item, the purchaser will complete the request and upload a receipt</li>
+      <li>The treasurer will submit the purchase for reimbursement from the Purdue Business Office for Student Organizations</li>
+    </ol>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Help',
+}
+</script>
