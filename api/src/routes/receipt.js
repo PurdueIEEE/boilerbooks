762642b6a198e1f8a82ee5953a763fb97416c27b
Hadi Ahmed <hadiahmed098@gmail.com>
2022-01-09 19:34:24 -0500

    Added receipt view check

--- api/src/routes/receipt.js
+++ api/src/routes/receipt.js
@@ -2,9 +2,38 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get('/:file', (req, res) => {
-    // TODO check to make sure that only purchaser, approvers, and treasurers can view receipts
-    res.download(process.env.RECEIPT_BASEDIR + '/receipt/' + req.params.file);
+router.get('/:file', async (req, res) => {
+    // make sure that only purchaser, approvers, and treasurers can view receipts
+    const id_from_file = /.*?_(?<pnum>[0-9]+)(_reupload_[0-9]+)?\.(png|jpg|pdf|jpeg)$/gm;
+    const match = id_from_file.exec(req.params.file);
+    if (match === null || match.groups.pnum === undefined || match.groups.pnum === null) {
+        return res.status(404).send("Receipt not found");
+    }
+
+    try {
+        const [results, fields]  = await req.context.models.purchase.getFullPurchaseByID(match.groups.pnum);
+        if (results.length === 0) {
+            return res.status(404).send("Receipt not found");
+        }
+        // user is purchaser
+        if (results[0].username === req.context.request_user_id) {
+            return res.download(process.env.RECEIPT_BASEDIR + '/receipt/' + req.params.file);
+        }
+        try {
+            const [results_1, fields_1]  = await req.context.models.account.getUserApprovals(req.context.request_user_id, results[0].committee);
+            if (results_1.length === 0) {
+                return res.status(404).send("Receipt not found");
+            }
+            // user has approval power for committee
+            return res.download(process.env.RECEIPT_BASEDIR + '/receipt/' + req.params.file);
+        } catch (err) {
+            console.log(err.stack);
+            return res.status(500).send("Internal Server Error");
+        }
+    } catch (err) {
+        console.log(err.stack);
+        return res.status(500).send("Internal Server Error");
+    }
 });
 
 export default router;
