ef9e40ed880f1c5c9f61ea06108fdb6382d2871c
Hadi Ahmed <hadiahmed098@gmail.com>
2022-02-28 21:18:03 -0500

    Removed cornerstones from committee dropdown

--- committees.php
+++ committees.php
@@ -3,11 +3,7 @@
 <option <?php echo @$committee === "Aerial Robotics"  ? "selected" : '' ?> value="Aerial Robotics">Aerial Robotics</option>
 <option <?php echo @$committee === "Computer Society" ? "selected" : '' ?> value="Computer Society">Computer Society</option>
 <option <?php echo @$committee === "EMBS"             ? "selected" : '' ?> value="EMBS">EMBS</option>
-<option <?php echo @$committee === "GE"               ? "selected" : '' ?> value="GE">Growth and Engagement</option>
 <option <?php echo @$committee === "MTT-S"            ? "selected" : '' ?> value="MTT-S">MTT-S</option>
-<option <?php echo @$committee === "Professional"     ? "selected" : '' ?> value="Professional">Professional</option>
-<option <?php echo @$committee === "Learning"         ? "selected" : '' ?> value="Learning">Learning</option>
 <option <?php echo @$committee === "Racing"           ? "selected" : '' ?> value="Racing">Racing</option>
 <option <?php echo @$committee === "ROV"              ? "selected" : '' ?> value="ROV">ROV</option>
-<option <?php echo @$committee === "Social"           ? "selected" : '' ?> value="Social">Social</option>
 <option <?php echo @$committee === "SOGA"             ? "selected" : '' ?> value="SOGA">SOGA</option>
