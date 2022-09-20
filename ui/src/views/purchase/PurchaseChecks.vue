e8fb2e81803f7c9b45175b9f7eb21e4313c30e98
Hadi Ahmed <hadiahmed098@gmail.com>
2022-09-19 22:52:18 -0400

    Capitalization is hard

--- ui/src/views/purchase/PurchaseChecks.vue
+++ ui/src/views/purchase/PurchaseChecks.vue
@@ -19,7 +19,7 @@
     >
       <template v-slot:data="purchase">
         <td>{{purchase.row.date}}</td>
-        <td><router-link v-bind:to="goToItem(purchase.row.purchaseid)" class="link-primary text-decoration-none">{{purchase.row.item}}</router-link></td>
+        <td><router-link v-bind:to="goToItem(purchase.row.purchaseID)" class="link-primary text-decoration-none">{{purchase.row.item}}</router-link></td>
         <td>{{purchase.row.vendor}}</td>
         <td>{{purchase.row.committee}}</td>
         <td>{{purchase.row.approvedby}}</td>
