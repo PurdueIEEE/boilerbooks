ca951aa5c8f95ad876e60289d54458781c1424a4
Hadi Ahmed <hadiahmed098@gmail.com>
2023-03-29 19:46:56 -0500

    New committee schema

--- config/sql-setup.sql
+++ config/sql-setup.sql
@@ -5,4 +5,6 @@ FLUSH PRIVILEGES;
 INSERT INTO `ieee-money`.`Users` (first, last, email, address, city, state, zip, cert, username, password, passwordreset, apikey)
     VALUES ('Purdue', 'IEEE', 'ieee-infrastructure@purdueieee.org', '465 Northwestern Ave', 'West Lafayette', 'IN', '47907', '', 'pieee', '$2b$10$2SwcYOGA2ltZqSZXMO3r0OunMr.Ff04rU0Hg7PxcUhCz1qZFwv2.W',  '', '');
 
-INSERT INTO `ieee-money`.`approval` (username, role, committee, amount, category, privilege_level) VALUES ('pieee', 'Master Account', 'General IEEE', '0', '*', '6');
+INSERT INTO `ieee-money`.`committees` (display_name, api_name, bank_status, dues_status) VALUES ('General IEEE', 'general', 'Active', 'Inactive');
+
+INSERT INTO `ieee-money`.`approval` (username, role, committee, amount, category, privilege_level) VALUES ('pieee', 'Master Account', 1, '0', '*', '6');
